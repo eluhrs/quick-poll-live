7095c2f95a62f70d9f9101dab11af5b6a55d0e91
Eric Luhrs <eluhrs@v159136.cc.lehigh.edu>
2026-01-20 12:22:22 -0500

    CLI: Update help text to reflect generic poll reassignment

--- bin/manage_users.sh
+++ bin/manage_users.sh
@@ -9,7 +9,7 @@ if [ "$#" -eq 0 ]; then
     echo "Examples:"
     echo "  $0 list                (List all users)"
     echo "  $0 admin newpassword   (Update 'admin' password or create new user)"
-    echo "  $0 delete olduser      (Delete 'olduser' and reassign polls to 'admin')"
+    echo "  $0 delete olduser      (Delete 'olduser' and reassign polls to another user)"
     exit 1
 fi
 
